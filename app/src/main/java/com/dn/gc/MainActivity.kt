2a53eb6f3496c30f00152cc42abc059c29ed3fe1
张晋明 <245545357@qq.com>
2021-12-31 18:35:59 +0800

    init

---
+++ app/src/main/java/com/dn/gc/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dn.gc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
