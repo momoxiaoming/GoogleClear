ce496db7db4a29c65eb699fec7956bf1d1494f1b
张晋明 <245545357@qq.com>
2022-07-21 22:10:03 +0800

    22721

--- biz/cleanup/vest/vest1/src/main/java/com/org/gradle/vest1/VestHeaderFragment.kt
+++ biz/cleanup/vest/vest1/src/main/java/com/org/gradle/vest1/VestHeaderFragment.kt
@@ -1,5 +1,8 @@
 package com.org.gradle.vest1
 
+import androidx.lifecycle.ViewModelProvider
+import com.mckj.baselib.base.databinding.DataBindingFragment
+
 import com.org.gradle.vest1.databinding.CleanupFragmentHeaderBinding
 
 /**
@@ -8,7 +11,25 @@ import com.org.gradle.vest1.databinding.CleanupFragmentHeaderBinding
  * @author mmxm
  * @date 2022/7/21 16:36
  */
-class VestHeaderFragment :DataBindingFragment<CleanupFragmentHeaderBinding, BaseHomeViewModel>() {
-    
+class VestHeaderFragment : DataBindingFragment<CleanupFragmentHeaderBinding,BaseHomeViewModel>(){
+
+    override fun getLayoutId(): Int {
+        return R.layout.cleanup_fragment_header
+    }
+
+    override fun getViewModel(): BaseHomeViewModel {
+        return ViewModelProvider(requireActivity()).get(
+            BaseHomeViewModel::class.java
+        )
+    }
+
+    override fun initData() {
+
+    }
+
+    override fun initView() {
+        //获取内存使用量
+    }
+
 
 }
